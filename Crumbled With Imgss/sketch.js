1d68b9c06e70235a078d2cdb137fa6a6810cc1f4
ishwaak <74703959+ishwaak@users.noreply.github.com>
2021-02-17 19:23:02 +0530

    Add files via upload

---
+++ Crumbled With Imgss/sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var box1,box2,box3,box4;
+var ground;
+
+function setup() {
+	createCanvas(1600,700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+dustBin = new dustbin(1200,650);
+paperObject = new Paper(200,450,70);
+grownd = new Ground(width/2, 670,width,20);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+  
+  dustBin.display();
+  paperObject.display();
+  grownd.display();
+
+
+
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+	}
+}
+
